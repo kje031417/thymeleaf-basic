b62f61b2aabf109b7b6b32bf137645312155700d
kje031417 <kje031417@gmail.com>
2023-03-29 16:33:04 +0900

    23.03.29 first commit

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -75,11 +75,6 @@ public class BasicController {
         return "basic/literal";
     }
 
-    @GetMapping("/attribute")
-    public String attribute() {
-        return "basic/attribute";
-    }
-
     @GetMapping("/operation")
     public String operation(Model model) {
         model.addAttribute("nullData", null);
@@ -87,8 +82,29 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
+
     @Component("helloBean")
     static class HelloBean {
+
         public String hello(String data) {
             return "Hello " + data;
         }
